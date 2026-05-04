9ae990cc1d5b50e750578afaaa6821b611d37e8b
Johan Marwin Manio <storgephobia@gmail.com>
2026-05-04 15:54:37 +0800

    Update build.gradle.kts for Java 25 and cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     `java-library`
     alias(libs.plugins.shadow)
-    alias(libs.plugins.paperweight.userdev)
-    alias(libs.plugins.runpaper)
 }
 
 group = "com.danikvitek"
@@ -29,62 +27,38 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
     compileOnly(libs.jetbrains.annotations)
     implementation(libs.bstats.bukkit)
 }
 
-val targetJavaVersion = 17
+val targetJavaVersion = 25
+
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
 }
-tasks.withType(xyz.jpenilla.runtask.task.AbstractRun::class) {
-    javaLauncher = javaToolchains.launcherFor {
-        vendor = JvmVendorSpec.JETBRAINS
-        languageVersion = JavaLanguageVersion.of(targetJavaVersion)
-    }
-    jvmArgs(
-        "-XX:+AllowEnhancedClassRedefinition",
-        "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005",
-//        "-Dpaper.log-level=ALL",
-//        "-Dlog4j.configurationFile=log4j2.xml"
-    )
-}
 
 tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
     shadowJar {
         relocate("org.bstats", "com.danikvitek.bstats")
     }
+
     build {
         dependsOn(shadowJar)
     }
-    runServer {
-        dependsOn(shadowJar)
-        minecraftVersion("1.18.2")
-        downloadPlugins {
-            modrinth("ViaVersion", "wMqhJcIA")
-            modrinth("LuckPerms", "ZPtLedoF")
-            modrinth("TabTPS", "DlhrDe98")
-        }
-    }
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
 
-        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-        // See https://openjdk.java.net/jeps/247 for more information.
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(targetJavaVersion)
     }
+
     javadoc {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+        options.encoding = Charsets.UTF_8.name()
     }
+
     processResources {
-        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-        val props = mapOf(
-            "version" to project.version,
-        )
+        filteringCharset = Charsets.UTF_8.name()
+        val props = mapOf("version" to project.version)
         inputs.properties(props)
         filesMatching("plugin.yml") {
             expand(props)
